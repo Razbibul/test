f79e92ee09caf1ad5aa0332d732cff89dfe631eb
Razbibul <bimmer02@yandex.ru>
2023-01-27 00:45:28 +0500

    new comment

--- test_items.py
+++ test_items.py
@@ -4,6 +4,6 @@ import time
 
 def test_basket(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
-    time.sleep(30) # держи слип братулёк :)
+    #time.sleep(30) # держи слип братулёк :)
     elem = browser.find_element(By.ID, "id_quantity")
     assert elem, 'видишь кнопку? и я нет'
